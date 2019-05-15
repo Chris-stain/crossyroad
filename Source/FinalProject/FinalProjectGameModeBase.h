9bc077e5fa5c04d9b6e24687b159f8b84070c4a6
Chris-stain <chris_jimenez93@hotmail.com>
2019-05-15 16:39:40 -0700

    Add files via upload

---
+++ Source/FinalProject/FinalProjectGameModeBase.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FinalProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FINALPROJECT_API AFinalProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	AFinalProjectGameModeBase();
+	
+	
+};
